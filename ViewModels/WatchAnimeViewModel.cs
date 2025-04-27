c63c2f5c18d5a94941a01d4e343bb93a79e59092
Jeremiasz Kaźmierczak <loiuy538@gmail.com>
2025-04-27 15:52:23 +0200

    added watch window and fixed search

---
+++ ViewModels/WatchAnimeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Aniki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aniki.ViewModels
+{
+    public partial class WatchAnimeViewModel : ViewModelBase
+    {
+        private AnimeDetails _details;
+
+        public WatchAnimeViewModel() { }
+
+        public WatchAnimeViewModel(AnimeDetails details)
+        {
+            _details = details;
+        }
+    }
+}
---
+++ Views/WatchAnimeView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Aniki.Views
+{
+    public partial class WatchAnimeView : UserControl
+    {
+        public WatchAnimeView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+            => AvaloniaXamlLoader.Load(this);
+    }
+}
