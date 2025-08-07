7c4475845d5e5fe2e71538396bd37ac9855f421e
jkazmierczakdbr77 <loiuy538@gmail.com>
2025-08-07 10:47:49 +0200

    fix

---
+++ Converters/BooleanInverterConverter.cs
@@ -0,0 +1,24 @@
+
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Aniki.Converters
+{
+    public class BooleanInverterConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolean)
+            {
+                return !boolean;
+            }
+            return value;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
