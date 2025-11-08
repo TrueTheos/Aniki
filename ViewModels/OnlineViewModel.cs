65a39bfe6fe45c89dd0a8dd4add5584f04a55dba
Theos <loiuy538@gmail.com>
2025-11-09 00:01:09 +0100

    clean

--- ViewModels/OnlineViewModel.cs
+++ ViewModels/OnlineViewModel.cs
@@ -11,7 +11,6 @@ public partial class OnlineViewModel : ViewModelBase, IDisposable
     private readonly IAllMangaScraperService _scraperService;
     private LibVLC? _libVLC;
     private MediaPlayer? _mediaPlayer;
-    private System.Timers.Timer? _progressTimer;
 
     [ObservableProperty]
     private string _searchQuery = "";
@@ -46,15 +45,6 @@ public partial class OnlineViewModel : ViewModelBase, IDisposable
     private bool _isLoading;
 
     [ObservableProperty]
-    private TimeSpan _currentTime;
-
-    [ObservableProperty]
-    private TimeSpan _duration;
-
-    [ObservableProperty]
-    private double _position;
-
-    [ObservableProperty]
     private bool _isFullscreen;
 
     public MediaPlayer? MediaPlayer => _mediaPlayer;
@@ -63,7 +53,6 @@ public partial class OnlineViewModel : ViewModelBase, IDisposable
     {
         _scraperService = scraperService;
         InitializeVLC();
-        InitializeProgressTimer();
     }
 
     private void InitializeVLC()
@@ -71,51 +60,6 @@ public partial class OnlineViewModel : ViewModelBase, IDisposable
         Core.Initialize();
         _libVLC = new LibVLC();
         _mediaPlayer = new MediaPlayer(_libVLC);
-        
-        // Subscribe to media player events
-        _mediaPlayer.TimeChanged += OnTimeChanged;
-        _mediaPlayer.LengthChanged += OnLengthChanged;
-    }
-
-    private void InitializeProgressTimer()
-    {
-        _progressTimer = new System.Timers.Timer(1000);
-        _progressTimer.Elapsed += OnProgressTimerElapsed;
-        _progressTimer.Start();
-    }
-
-    private void OnTimeChanged(object? sender, MediaPlayerTimeChangedEventArgs e)
-    {
-        CurrentTime = TimeSpan.FromMilliseconds(e.Time);
-        if (Duration.TotalMilliseconds > 0)
-        {
-            Position = e.Time;
-        }
-    }
-
-    private void OnLengthChanged(object? sender, MediaPlayerLengthChangedEventArgs e)
-    {
-        Duration = TimeSpan.FromMilliseconds(e.Length);
-    }
-
-    private void OnProgressTimerElapsed(object? sender, ElapsedEventArgs e)
-    {
-        if (_mediaPlayer != null && _mediaPlayer.IsPlaying)
-        {
-            CurrentTime = TimeSpan.FromMilliseconds(_mediaPlayer.Time);
-            if (_mediaPlayer.Length > 0)
-            {
-                Position = _mediaPlayer.Time;
-            }
-        }
-    }
-
-    partial void OnPositionChanged(double value)
-    {
-        if (_mediaPlayer != null && Math.Abs(_mediaPlayer.Time - value) > 1000)
-        {
-            _mediaPlayer.Time = (long)value;
-        }
     }
 
     [RelayCommand]
@@ -243,16 +187,7 @@ public partial class OnlineViewModel : ViewModelBase, IDisposable
 
     public void Dispose()
     {
-        _progressTimer?.Stop();
-        _progressTimer?.Dispose();
-        
-        if (_mediaPlayer != null)
-        {
-            _mediaPlayer.TimeChanged -= OnTimeChanged;
-            _mediaPlayer.LengthChanged -= OnLengthChanged;
-            _mediaPlayer.Dispose();
-        }
-        
+        _mediaPlayer?.Dispose();
         _libVLC?.Dispose();
     }
 }
